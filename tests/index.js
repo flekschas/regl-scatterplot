009b7805611b51c2b668f0fe3680af60c5ab53c8
Fritz Lekschas <service@lekschas.de>
2018-11-01 09:39:10 -0400

    Test default values. Add test titles.

--- tests/index.js
+++ tests/index.js
@@ -1,40 +1,61 @@
+/* eslint no-console: 1 */
+
 import test from "tape";
 import browserEnv from "browser-env";
 import createRegl from "regl";
 
-// import createScatterplot from "../src";
-// import DEFAULT from '../src/defaults';
+browserEnv();
+
+import createScatterplot from "../src";
+import DEFAULT from "../src/defaults";
 
 import createCanvas from "./create-canvas";
-
-browserEnv();
 
 test("creates a Regl instance with a fake canvas element with a webgl context", t => {
   t.plan(3);
 
-  const canvas = createCanvas(200, 200);
+  const dim = 200;
+  const canvas = createCanvas(dim, dim);
   const gl = canvas.getContext("webgl");
 
-  t.ok(gl.drawingBufferWidth === 200);
-  t.ok(gl.drawingBufferHeight === 200);
+  t.ok(gl.drawingBufferWidth === dim, `width should be ${dim}px`);
+  t.ok(gl.drawingBufferHeight === dim, `height should be ${dim}px`);
 
   const regl = createRegl({ canvas });
 
-  t.ok(!!regl);
+  t.ok(!!regl, "regl should be instanciated");
 });
 
-// test("creates scatterplot with default values", t => {
-//   t.plan(7);
+test("creates scatterplot with default values", t => {
+  t.plan(7);
 
-//   const canvas = createCanvas(200, 200);
-//   const scatterplot = createScatterplot({ canvas });
+  const dim = 200;
+  const canvas = createCanvas(dim, dim);
+  const scatterplot = createScatterplot({ canvas });
 
-//   t.ok(scatterplot.canvas === canvas);
-
-//   t.ok(scatterplot.colorMap === DEFAULT.COLOR_MAP);
-//   t.ok(scatterplot.pointSize === DEFAULT.POINT_SIZE);
-//   t.ok(scatterplot.pointSizeSelected === DEFAULT.POINT_SIZE_SELECTED);
-//   t.ok(scatterplot.pointOutlineWidth === DEFAULT.POINT_OUTLINE_WIDTH);
-//   t.ok(scatterplot.width === DEFAULT.WIDTH);
-//   t.ok(scatterplot.height === DEFAULT.HEIGHT);
-// });
+  t.ok(scatterplot.canvas === canvas, "canvas object should equal");
+  t.ok(
+    scatterplot.colors === DEFAULT.COLORS,
+    "scatterplot should have default colors"
+  );
+  t.ok(
+    scatterplot.pointSize === DEFAULT.POINT_SIZE,
+    "scatterplot should have default point size"
+  );
+  t.ok(
+    scatterplot.pointSizeSelected === DEFAULT.POINT_SIZE_SELECTED,
+    "scatterplot should have default selected point size"
+  );
+  t.ok(
+    scatterplot.pointOutlineWidth === DEFAULT.POINT_OUTLINE_WIDTH,
+    "scatterplot should have default point outline width"
+  );
+  t.ok(
+    scatterplot.width === DEFAULT.WIDTH,
+    "scatterplot should have default width"
+  );
+  t.ok(
+    scatterplot.height === DEFAULT.HEIGHT,
+    "scatterplot should have default height"
+  );
+});
