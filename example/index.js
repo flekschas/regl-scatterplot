07baffad0699fb4222095b3743f9a04bef6b5832
Fritz Lekschas <service@lekschas.de>
2018-11-01 20:36:54 -0400

    Demonstrate a colormap with

--- example/index.js
+++ example/index.js
@@ -1,3 +1,5 @@
+/* eslint no-console: 0 */
+
 import createScatterplot from "../src";
 
 const canvas = document.querySelector("#canvas");
@@ -20,15 +22,17 @@ const resizeHandler = () => {
 
 window.addEventListener("resize", resizeHandler);
 
+let points = [];
 let selection = [];
 
-const selectHandler = ({ points }) => {
-  console.log("Selected:", points); // eslint-disable-line
-  selection = points;
+const selectHandler = ({ points: selectedPoints }) => {
+  console.log("Selected:", selectedPoints);
+  selection = selectedPoints;
+  if (selection.length === 1) console.log(points[selection[0]].slice(2));
 };
 
 const deselectHandler = () => {
-  console.log("Deselected:", selection); // eslint-disable-line
+  console.log("Deselected:", selection);
   selection = [];
 };
 
@@ -43,9 +47,12 @@ scatterplot.subscribe("select", selectHandler);
 scatterplot.subscribe("deselect", deselectHandler);
 
 const generatePoints = num =>
-  new Array(num)
-    .fill()
-    .map(() => [-1 + Math.random() * 2, -1 + Math.random() * 2]);
+  new Array(num).fill().map(() => [
+    -1 + Math.random() * 2, // x
+    -1 + Math.random() * 2, // y
+    Math.round(Math.random()), // category
+    Math.random() // value
+  ]);
 
 const numPointsInputHandler = event => {
   numPointsValEl.innerHTML = `${+event.target
@@ -71,7 +78,7 @@ pointSizeEl.addEventListener("input", pointSizeInputHandler);
 
 const opacityInputHandler = event => {
   opacityValEl.innerHTML = +event.target.value;
-  color[3] = +event.target.value;
+  scatterplot.style({ opacity: +event.target.value });
   scatterplot.draw();
 };
 
@@ -83,4 +90,114 @@ const resetClickHandler = () => {
 
 resetEl.addEventListener("click", resetClickHandler);
 
-scatterplot.draw(generatePoints(1000));
+scatterplot.colors = [
+  ["#3a78aa", "#008dff", "#008dff"],
+  ["#aa3a99", "#ff00da", "#ff00da"]
+];
+scatterplot.colorBy("category");
+
+scatterplot.colors = [
+  "#8b0000",
+  "#8e0006",
+  "#91000a",
+  "#94010e",
+  "#970212",
+  "#990215",
+  "#9d0419",
+  "#a0051c",
+  "#a2071f",
+  "#a40921",
+  "#a70b24",
+  "#aa0e27",
+  "#ac1029",
+  "#b0122c",
+  "#b2152e",
+  "#b41731",
+  "#b71933",
+  "#ba1c35",
+  "#bb1e37",
+  "#be213a",
+  "#c0233c",
+  "#c2263d",
+  "#c52940",
+  "#c72b42",
+  "#ca2e43",
+  "#cc3045",
+  "#cd3346",
+  "#cf3548",
+  "#d2384a",
+  "#d43b4b",
+  "#d53d4d",
+  "#d8404e",
+  "#d9434f",
+  "#db4551",
+  "#dd4852",
+  "#df4a53",
+  "#e14d54",
+  "#e35056",
+  "#e45356",
+  "#e55658",
+  "#e75859",
+  "#e85b59",
+  "#ea5e5b",
+  "#ec615b",
+  "#ed645c",
+  "#ee665d",
+  "#f0695e",
+  "#f16c5f",
+  "#f37060",
+  "#f37261",
+  "#f57562",
+  "#f67862",
+  "#f77a63",
+  "#f87d64",
+  "#f98065",
+  "#fa8266",
+  "#fb8567",
+  "#fc8868",
+  "#fd8b69",
+  "#fe8f6a",
+  "#fe916b",
+  "#ff956c",
+  "#ff986e",
+  "#ff9b6f",
+  "#ff9e70",
+  "#ffa172",
+  "#ffa474",
+  "#ffa775",
+  "#ffaa77",
+  "#ffad79",
+  "#ffb17b",
+  "#ffb47d",
+  "#ffb67f",
+  "#ffb981",
+  "#ffbd83",
+  "#ffbf86",
+  "#ffc288",
+  "#ffc58a",
+  "#ffc88d",
+  "#ffca90",
+  "#ffcd93",
+  "#ffd196",
+  "#ffd399",
+  "#ffd69c",
+  "#ffd9a0",
+  "#ffdba4",
+  "#ffdda7",
+  "#ffe0ab",
+  "#ffe3af",
+  "#ffe5b2",
+  "#ffe9b7",
+  "#ffebba",
+  "#ffedbf",
+  "#fff0c4",
+  "#fff2c8",
+  "#fff5cd",
+  "#fff7d1",
+  "#fffad7",
+  "#fffddb",
+  "#ffffe0"
+];
+scatterplot.colorBy("value");
+points = generatePoints(1000);
+scatterplot.draw(points);
