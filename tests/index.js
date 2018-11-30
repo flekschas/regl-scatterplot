dde1c62bf6d21401ce4178ce79ac0c5e568a6133
Fritz Lekschas <service@lekschas.de>
2018-11-30 17:59:40 -0500

    Add test for attr({ width, height, aspectRatio }) and style({ background }). Use `t.equal` if possibe. Switch to single quotes

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,9 @@
 /* eslint no-console: 1 */
 
-import test from "zora";
-import createRegl from "regl";
+import test from 'zora';
+import createRegl from 'regl';
 
-import createScatterplot from "../src";
+import createScatterplot from '../src';
 import {
   COLORS,
   HEIGHT,
@@ -11,61 +11,118 @@ import {
   POINT_SIZE,
   POINT_SIZE_SELECTED,
   WIDTH
-} from "../src/defaults";
+} from '../src/defaults';
 
-const createCanvas = (width, height) => {
-  const canvas = document.createElement("canvas");
+const createCanvas = (width = 200, height = 200) => {
+  const canvas = document.createElement('canvas');
   canvas.width = width;
   canvas.height = height;
   return canvas;
 };
 
-test("creates a Regl instance with a fake canvas element with a webgl context", t => {
+test('creates a Regl instance with a fake canvas element with a webgl context', t => {
   const dim = 200;
   const canvas = createCanvas(dim, dim);
-  const gl = canvas.getContext("webgl");
+  const gl = canvas.getContext('webgl');
 
-  t.ok(gl.drawingBufferWidth === dim, `width should be ${dim}px`);
-  t.ok(gl.drawingBufferHeight === dim, `height should be ${dim}px`);
+  t.equal(gl.drawingBufferWidth, dim, `width should be ${dim}px`);
+  t.equal(gl.drawingBufferHeight, dim, `height should be ${dim}px`);
 
   const regl = createRegl({ canvas });
 
-  t.ok(!!regl, "regl should be instanciated");
+  t.ok(!!regl, 'regl should be instanciated');
 });
 
-test("creates scatterplot with default values", t => {
-  const canvas = createCanvas(200, 200);
+test('creates scatterplot with default values', t => {
+  const canvas = createCanvas();
   const scatterplot = createScatterplot({ canvas });
 
-  t.ok(scatterplot.canvas === canvas, "canvas object should equal");
-  t.ok(
-    scatterplot.style("colors") === COLORS,
-    "scatterplot should have default colors"
+  t.equal(scatterplot.canvas, canvas, 'canvas object should equal');
+  t.equal(
+    scatterplot.style('colors'),
+    COLORS,
+    'scatterplot should have default colors'
   );
-  t.ok(
-    scatterplot.style("pointSize") === POINT_SIZE,
-    "scatterplot should have default point size"
+  t.equal(
+    scatterplot.style('pointSize'),
+    POINT_SIZE,
+    'scatterplot should have default point size'
   );
-  t.ok(
-    scatterplot.style("pointSizeSelected") === POINT_SIZE_SELECTED,
-    "scatterplot should have default selected point size"
+  t.equal(
+    scatterplot.style('pointSizeSelected'),
+    POINT_SIZE_SELECTED,
+    'scatterplot should have default selected point size'
   );
-  t.ok(
-    scatterplot.style("pointOutlineWidth") === POINT_OUTLINE_WIDTH,
-    "scatterplot should have default point outline width"
+  t.equal(
+    scatterplot.style('pointOutlineWidth'),
+    POINT_OUTLINE_WIDTH,
+    'scatterplot should have default point outline width'
   );
-  t.ok(
-    scatterplot.attr("width") === WIDTH,
-    "scatterplot should have default width"
+  t.equal(
+    scatterplot.attr('width'),
+    WIDTH,
+    'scatterplot should have default width'
   );
-  t.ok(
-    scatterplot.attr("height") === HEIGHT,
-    "scatterplot should have default height"
+  t.equal(
+    scatterplot.attr('height'),
+    HEIGHT,
+    'scatterplot should have default height'
   );
 });
 
-test("creates colormap", t => {
-  const canvas = createCanvas(200, 200);
+/* --------------------------------- attr() --------------------------------- */
+
+test('set width and height via attr()', t => {
+  const w1 = 200;
+  const h1 = 200;
+
+  const canvas = createCanvas(w1, h1);
+  const gl = canvas.getContext('webgl');
+  const scatterplot = createScatterplot({ canvas, width: w1, height: h1 });
+
+  t.equal(gl.drawingBufferWidth, w1, `width should be ${w1}px`);
+  t.equal(gl.drawingBufferHeight, h1, `height should be ${h1}px`);
+
+  const w2 = 400;
+  const h2 = 300;
+
+  scatterplot.attr({ width: w2, height: h2 });
+
+  t.equal(gl.drawingBufferWidth, w2, `width should be ${w2}px`);
+  t.equal(gl.drawingBufferHeight, h2, `height should be ${h2}px`);
+});
+
+test('set aspectRatio via attr()', t => {
+  const canvas = createCanvas(400, 200);
+  const scatterplot = createScatterplot({ canvas, width: 400, height: 200 });
+
+  const aspectRatio = 2;
+  scatterplot.attr({ aspectRatio });
+
+  t.equal(
+    scatterplot.attr('aspectRatio'),
+    aspectRatio,
+    `data aspect ratio should be ${aspectRatio}`
+  );
+});
+
+/* --------------------------------- style() -------------------------------- */
+
+test('set background via style()', t => {
+  const scatterplot = createScatterplot({ canvas: createCanvas() });
+
+  const backgroundHex = '#ff0000';
+  const backgroundNrgba = [1, 0, 0, 1];
+  scatterplot.style({ background: backgroundHex });
+
+  t.ok(
+    scatterplot.style('background').every((v, i) => v === backgroundNrgba[i]),
+    'background should be red and and converted to normalized RGBA'
+  );
+});
+
+test('set colors via style()', t => {
+  const canvas = createCanvas();
   const scatterplot = createScatterplot({ canvas });
 
   const rgba6 = [
@@ -103,40 +160,40 @@ test("creates colormap", t => {
 
   scatterplot.style({
     colors: [
-      ["#3a78aa", "#008dff", "#008dff"],
-      ["#aa3a99", "#ff00da", "#ff00da"]
+      ['#3a78aa', '#008dff', '#008dff'],
+      ['#aa3a99', '#ff00da', '#ff00da']
     ]
   });
 
   t.ok(
     scatterplot
-      .style("colors")
+      .style('colors')
       .every((color, i) => color.every((c, j) => c === rgba6[i][j])),
-    "should create 8 normalized RGBAs from 6 HEX"
+    'should create 8 normalized RGBAs from 6 HEX'
   );
 
-  scatterplot.style({ colors: ["#008dff", "#ff00da"] });
+  scatterplot.style({ colors: ['#008dff', '#ff00da'] });
 
   t.ok(
     scatterplot
-      .style("colors")
+      .style('colors')
       .every((color, i) => color.every((c, j) => c === rgba2[i][j])),
-    "should create 8 normalized RGBAs from 2 HEX"
+    'should create 8 normalized RGBAs from 2 HEX'
   );
 
   scatterplot.style({ colors: [[0, 141, 255], [255, 0, 218]] });
   t.ok(
     scatterplot
-      .style("colors")
+      .style('colors')
       .every((color, i) => color.every((c, j) => c === rgba2[i][j])),
-    "should create 8 normalized RGBAs from 2 non-normalized RGB"
+    'should create 8 normalized RGBAs from 2 non-normalized RGB'
   );
 
   scatterplot.style({ colors: [[0, 141, 255, 127], [255, 0, 218, 127]] });
   t.ok(
     scatterplot
-      .style("colors")
+      .style('colors')
       .every((color, i) => color.every((c, j) => c === rgba2a[i][j])),
-    "should create 8 normalized RGBAs from 2 non-normalized RGBAs"
+    'should create 8 normalized RGBAs from 2 non-normalized RGBAs'
   );
 });
