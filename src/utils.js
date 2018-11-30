4db4e847cdf475e27724eda5fa7ea80f5a94d157
Fritz Lekschas <service@lekschas.de>
2018-11-30 11:35:54 -0500

    Custom min max implementations as they are more performant

--- src/utils.js
+++ src/utils.js
@@ -172,6 +172,26 @@ export const isRgba = rgba =>
 export const arrayMax = (max, x) => (max > x ? max : x);
 
 /**
+ * Fast version of `Math.max`. Based on
+ *   https://jsperf.com/math-min-max-vs-ternary-vs-if/24 `Math.max` is not
+ *   very fast
+ * @param   {number}  a  Value A
+ * @param   {number}  b  Value B
+ * @return  {boolean}  If `true` A is greater than B.
+ */
+export const max = (a, b) => (a > b ? a : b);
+
+/**
+ * Fast version of `Math.min`. Based on
+ *   https://jsperf.com/math-min-max-vs-ternary-vs-if/24 `Math.max` is not
+ *   very fast
+ * @param   {number}  a  Value A
+ * @param   {number}  b  Value B
+ * @return  {boolean}  If `true` A is smaller than B.
+ */
+export const min = (a, b) => (a < b ? a : b);
+
+/**
  * Normalize an array
  * @param   {array}  a  Array to be normalized.
  * @return  {array}  Normalized array.
