29094855264963d398b66c39a011b6b1e9ce6fe3
Fritz Lekschas <service@lekschas.de>
2018-12-02 14:57:36 -0500

    Add doc strings

--- src/utils.js
+++ src/utils.js
@@ -13,6 +13,11 @@ import { GL_EXTENSIONS } from './constants';
 export const dist = (x1, y1, x2, y2) =>
   Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 
+/**
+ * Create a new Regl instance with `GL_EXTENSIONS` enables
+ * @param   {object}  canvas  Canvas element to be rendered on
+ * @return  {function}  New Regl instance
+ */
 export const createRegl = canvas => {
   const gl = canvas.getContext('webgl');
   const extensions = [];
@@ -32,6 +37,11 @@ export const createRegl = canvas => {
   return createOriginalRegl({ gl, extensions });
 };
 
+/**
+ * Check if all GL extensions are enabled and warn otherwise
+ * @param   {function}  regl  Regl instance to be tested
+ * @return  {function}  Returns the Regl instance itself
+ */
 export const checkReglExtensions = regl => {
   if (!regl) return regl;
   GL_EXTENSIONS.forEach(EXTENSION => {
@@ -44,17 +54,23 @@ export const checkReglExtensions = regl => {
   return regl;
 };
 
-export const getBBox = pos => {
+/**
+ * Get the bounding box of a set of 2D positions
+ * @param   {array}  positions2d  2D positions to be checked
+ * @return  {array}  Quadruple of form `[xMin, yMin, xMax, yMax]` defining the
+ *  bounding box
+ */
+export const getBBox = positions2d => {
   let xMin = Infinity;
   let xMax = -Infinity;
   let yMin = Infinity;
   let yMax = -Infinity;
 
-  for (let i = 0; i < pos.length; i += 2) {
-    xMin = pos[i] < xMin ? pos[i] : xMin;
-    xMax = pos[i] > xMax ? pos[i] : xMax;
-    yMin = pos[i + 1] < yMin ? pos[i + 1] : yMin;
-    yMax = pos[i + 1] > yMax ? pos[i + 1] : yMax;
+  for (let i = 0; i < positions2d.length; i += 2) {
+    xMin = positions2d[i] < xMin ? positions2d[i] : xMin;
+    xMax = positions2d[i] > xMax ? positions2d[i] : xMax;
+    yMin = positions2d[i + 1] < yMin ? positions2d[i + 1] : yMin;
+    yMax = positions2d[i + 1] > yMax ? positions2d[i + 1] : yMax;
   }
 
   return [xMin, yMin, xMax, yMax];
