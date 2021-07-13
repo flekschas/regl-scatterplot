2265432282f2f2e83cc95b926ab19bb746b4d771
Trevor Manz <trevor.j.manz@gmail.com>
2021-07-13 16:28:25 -0400

    extend virtual html template plugin

--- vite.config.js
+++ vite.config.js
@@ -1,61 +1,69 @@
 import { defineConfig } from 'vite';
 import virtualHtmlTemplate from 'vite-plugin-virtual-html-template';
 
-const chunks = [
-  'index',
-  'axes',
-  'connected-points-by-segments',
-  'connected-points',
-  'dynamic-opacity',
-  'embedded',
-  'performance-mode',
-  'size-encoding',
-  'texture-background',
-  'transition',
-  'two-instances',
-];
+const htmlPlugin = ({ chunks, isDev }) => {
+  /**
+   * `vite-plugin-virtual-html-template` intercepts & handles requests for html
+   * from the client. Vite normally handles these requests and injects a script
+   * tag during dev (with a client runtime for HMR).
+   *
+   * The plugin uses `lodash.template` to runder the HTML, so a `<%= vite %>`
+   * tag is replaced with the missing vite client during dev. In prod, nothing is
+   * added.
+   */
+  const vite = isDev
+    ? '<script type="module" src="/@vite/client"></script>'
+    : '';
+  const pages = Object.fromEntries(
+    chunks.map((c) => [c, { entry: `example/${c}.js` }])
+  );
+  return {
+    ...virtualHtmlTemplate({ pages, data: { vite } }),
+    handleHotUpdate({ server }) {
+      // force auto-reload for changes
+      server.ws.send({ type: 'full-reload' });
+    },
+    config(config) {
+      if (!isDev) {
+        config.build.rollupOptions = {
+          ...config.build.rollupOptions,
+          input: Object.fromEntries(chunks.map((c) => [c, `${c}.html`])),
+        };
+      }
+    },
+  };
+};
 
-const pages = Object.fromEntries(
-  chunks.map((chunk) => [
-    chunk,
-    { template: 'public/index.html', entry: `example/${chunk}.js` },
-  ])
-);
-
-export default defineConfig({
-  base: './',
-  plugins: [
-    virtualHtmlTemplate({ pages }),
-    {
-      name: 'inject-vite-client',
-      apply: 'serve', // only for dev server
-      transform(code, id) {
-        if (id.includes('example')) {
-          return `import "/@vite/client";\n${code}`;
-        }
-        return null;
+export default ({ command }) =>
+  defineConfig({
+    base: './',
+    plugins: [
+      htmlPlugin({
+        chunks: [
+          'index',
+          'axes',
+          'connected-points-by-segments',
+          'connected-points',
+          'dynamic-opacity',
+          'embedded',
+          'performance-mode',
+          'size-encoding',
+          'texture-background',
+          'transition',
+          'two-instances',
+        ],
+        isDev: command === 'serve',
+      }),
+    ],
+    build: { outDir: 'docs' },
+    resolve: {
+      alias: {
+        /**
+         * vite pre-bundling (esbuild) can't be configured to
+         * resolve .fs/.vs in regl-line. This alias forces
+         * resolution with rollup, which avoids this error.
+         */
+        'regl-line': '/node_modules/regl-line/src/index.js',
       },
-      handleHotUpdate({ server }) {
-        server.ws.send({ type: 'full-reload' });
-      },
     },
-  ],
-  build: {
-    outDir: 'docs',
-    rollupOptions: {
-      input: Object.fromEntries(
-        chunks.map((chunk) => [chunk, `${chunk}.html`])
-      ),
-    },
-  },
-  resolve: {
-    alias: {
-      /**
-       * vite pre-bundling (esbuild) can't be configured to
-       * resolve .fs/.vs in regl-line. This alias forces
-       * resolution with rollup, which avoids this error.
-       */
-      'regl-line': '/node_modules/regl-line/src/index.js',
-    },
-  },
-});
+  });
