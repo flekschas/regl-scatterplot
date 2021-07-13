08591f4666054079872759cb852b071b67cca221
Trevor Manz <trevor.j.manz@gmail.com>
2021-07-13 17:23:19 -0400

    break up plugins

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import virtualHtmlTemplate from 'vite-plugin-virtual-html-template';
 
+
+const chunks = [
+  'index',
+  'axes',
+  'connected-points-by-segments',
+  'connected-points',
+  'dynamic-opacity',
+  'embedded',
+  'performance-mode',
+  'size-encoding',
+  'texture-background',
+  'transition',
+  'two-instances',
+];
+
 /** @returns {import('vite').Plugin} */
-const htmlPlugin = ({ chunks }) => {
+const htmlPlugin = ({ chunks, isDev }) => {
   /**
    * `vite-plugin-virtual-html-template` intercepts & handles requests for html
    * from the client. Vite normally handles these requests and injects a script
@@ -12,56 +27,32 @@ const htmlPlugin = ({ chunks }) => {
    * tag is replaced with the missing vite client during dev. In prod, nothing is
    * added.
    */
-  let plugin;
-  const init = (isDev) => {
-    const vite = isDev
-      ? '<script type="module" src="/@vite/client"></script>'
-      : '';
-    const pages = Object.fromEntries(
-      chunks.map((c) => [c, { entry: `example/${c}.js` }])
-    );
-    return virtualHtmlTemplate({ pages, data: { vite } });
-  };
-  return {
-    config(config, { command }) {
-      if (command === 'build') {
-        config.build.rollupOptions = {
-          ...config.build.rollupOptions,
-          input: Object.fromEntries(chunks.map((c) => [c, `${c}.html`])),
-        };
-      }
-      plugin = init(command === 'serve');
-    },
-    handleHotUpdate({ server }) {
-      // force auto refresh
-      server.ws.send({ type: 'full-reload' });
-    },
-    configureServer: (server) => plugin.configureServer(server),
-    resolveId: (id) => plugin.resolveId(id),
-    load: (id) => plugin.load(id),
-  };
+  const vite = isDev
+    ? '<script type="module" src="/@vite/client"></script>'
+    : '';
+  const pages = Object.fromEntries(
+    chunks.map((c) => [c, { entry: `example/${c}.js` }])
+  );
+  return virtualHtmlTemplate({ pages, data: { vite } });
 };
 
-export default defineConfig({
+export default ({ command }) => defineConfig({
   base: './',
   plugins: [
-    htmlPlugin({
-      chunks: [
-        'index',
-        'axes',
-        'connected-points-by-segments',
-        'connected-points',
-        'dynamic-opacity',
-        'embedded',
-        'performance-mode',
-        'size-encoding',
-        'texture-background',
-        'transition',
-        'two-instances',
-      ],
-    }),
+    htmlPlugin({ chunks, isDev: command === 'serve' }),
+    {
+      name: 'simple-reload',
+      handleHotUpdate({ server }) {
+        server.ws.send({ type: 'full-reload' });
+      }
+    }
   ],
-  build: { outDir: 'docs' },
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      input: Object.fromEntries(chunks.map((c) => [c, `${c}.html`])),
+    }
+  },
   resolve: {
     alias: {
       /**
