57aa62891259a2cfd042f49043367b8da3952c12
Mihail Yonchev <45242072+insertmike@users.noreply.github.com>
2023-04-27 09:33:11 +0200

    Update src/index.js
    Co-authored-by: Fritz Lekschas <code@lekschas.de>

--- src/index.js
+++ src/index.js
@@ -2409,9 +2409,7 @@ const createScatterplot = (
    * @returns {Promise<void>}
    */
   const zoomToPoints = (pointIdxs, options = {}) => {
-    if(!searchIndex){
-      return false;
-    }
+    if (!isInit) return Promise.reject(new Error(ERROR_NOT_INITIALIZED))
     const rect = getBBoxOfPoints(pointIdxs);
     const cX = rect.x + rect.width / 2;
     const cY = rect.y + rect.height / 2;
